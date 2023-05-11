aec3c75b68d231fed1f1756169f68ced75931fe9
John Davis <103592957+johndavis1999@users.noreply.github.com>
2023-05-12 01:48:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,87 @@
 <?php 
-$identificacion = '0931811087';
-function validar_identificacion_ecuador($identificacion) {
-    $cedula = trim($identificacion);
-    if (strlen($cedula) == 10 && is_numeric($cedula)) {
-        $cedula = $cedula;
-        $digito_region = substr($cedula, 0, 2);
-        if ($digito_region >= 1 && $digito_region <= 24) {
-            $ultimo_digito = substr($cedula, -1, 1);
-            $num_pares = 0;
-            $suma_impares = 0;
-            for ($i = 0; $i < 9; $i += 2) {
-                $num_pares += substr($cedula, $i, 1);
-            }
-            for ($i = 1; $i < 9; $i += 2) {
-                $suma_impares += substr($cedula, $i, 1);
-            }
-            $suma_total = ($num_pares * 2) + $suma_impares;
-            $primer_digito_suma = substr($suma_total, 0, 1);
-            $decena = ($primer_digito_suma + 1) * 10;
-            $digito_verificador = $decena - $suma_total;
-            if ($digito_verificador == 10) {
-                $digito_verificador = 0;
-            }
-            if ($digito_verificador == $ultimo_digito) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
+$identificacion = '0923896591';
 
+function validar_identificacion_ecuador($identificacion) {
+    // Eliminar cualquier caracter que no sea dígito
+    $identificacion = preg_replace('/[^0-9]/', '', $identificacion);
+
+    // Preguntamos si la identificación consta de 10 dígitos
+    if(strlen($identificacion) == 10) {
+        // Obtenemos el dígito de la región, que son los dos primeros dígitos
+        $digito_region = substr($identificacion, 0, 2);
+
+        // Preguntamos si la región existe (Ecuador se divide en 24 regiones)
+        if($digito_region >= 1 && $digito_region <= 24) {
+            // Extraemos el último dígito
+            $ultimo_digito = substr($identificacion, 9, 1);
+
+            // Agrupamos todos los pares y los sumamos
+            $pares = intval($identificacion[1]) + intval($identificacion[3]) + intval($identificacion[5]) + intval($identificacion[7]);
+
+            // Agrupamos los impares, los multiplicamos por un factor de 2
+            // Si el número resultante es mayor que 9, le restamos 9 al resultado
+            $numero1 = intval($identificacion[0]) * 2;
+            if ($numero1 > 9) {
+                $numero1 -= 9;
+            }
+
+            $numero3 = intval($identificacion[2]) * 2;
+            if ($numero3 > 9) {
+                $numero3 -= 9;
+            }
+
+            $numero5 = intval($identificacion[4]) * 2;
+            if ($numero5 > 9) {
+                $numero5 -= 9;
+            }
+
+            $numero7 = intval($identificacion[6]) * 2;
+            if ($numero7 > 9) {
+                $numero7 -= 9;
+            }
+
+            $numero9 = intval($identificacion[8]) * 2;
+            if ($numero9 > 9) {
+                $numero9 -= 9;
+            }
+
+            $impares = $numero1 + $numero3 + $numero5 + $numero7 + $numero9;
+
+            // Suma total
+            $suma_total = $pares + $impares;
+
+            // Extraemos el primer dígito de la suma (sumaPares + sumaImpares)
+            $primer_digito_suma = intval(substr(strval($suma_total), 0, 1));
+
+            // Obtenemos la decena inmediata
+            $decena = ($primer_digito_suma + 1) * 10;
+
+            // Obtenemos la resta de la decena inmediata - suma
+            // Si la suma nos da 10, el décimo dígito es cero
+            $digito_validador = $decena - $suma_total;
+            if ($digito_validador == 10) {
+                $digito_validador = 0;
+            }
+
+            // Validamos que el dígito validador sea igual al último dígito de la identificación
+            if ($digito_validador === intval($ultimo_digito)) {
+                return true; // Identificación válida
+            } else {
+                return false; // Identificación inválida
+            }
+        } else {
+            return false; // Región no válida
+        }
+    } else {
+         false; // Longitud incorrecta de la identificación
+    }
+}
+               
+
+
+  
 $cedulaValida = validar_identificacion_ecuador($identificacion);
-if ( $cedulaValida == false ) {
+if ( $cedulaValida != true ) {
 echo "<h1>Cédula válida</h1>";
 } else {
     echo "<h1>Cédula inválida</h1>";
